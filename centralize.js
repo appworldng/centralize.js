1a380dbc4340a0451d992c07c1d8563846e153d4
Chigozie Orunta <chigozieorunta@yahoo.com>
2019-10-18 12:00:02 +0100

    Update centralize.js

--- centralize.js
+++ centralize.js
@@ -1,1 +1,20 @@
-
+window.addEventListener("load", function() {
+  //Get elements with class centralize
+  var divs = document.getElementsByClassName("centralize");
+  
+  //Loop through elements
+  var i; for (i = 0; i < divs.length; i++) {
+    //Create child element
+    var childElement = document.createElement("div");
+    
+    //Set text node with parent element's node
+    var childElementContent = document.createTextNode(divs[i].textContent);
+    
+    //Assign child content to child elemnet
+    childElement.appendChild(childElementContent);
+    
+    //Assign child to parent
+    divs[i].appendChild(childElement);  
+  }
+  
+});
